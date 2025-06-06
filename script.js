d5e35356ca9e257a89bc6bb3237011c63c40655d
BellezaNatural <ledesma.mateo39@gmail.com>
2025-06-05 22:42:32 -0300

    15

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.querySelectorAll("button").forEach((btn) => {
     alert("Gracias por tu Mensaje 💄");
   });
 });
+
+const slider = document.getElementById('slider');
+const despues = document.querySelector('.despues');
+
+slider.addEventListener('input', (event) => {
+    let value = event.target.value;
+    despues.style.clipPath = `inset(0 ${100 - value}% 0 0)`;
+});
